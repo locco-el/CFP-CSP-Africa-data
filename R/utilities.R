e06491a5154c6bdbefe1564ad4fb6b391ec8938f
Oduro Oppong-Nkrumah <locco.el@gmail.com>
2020-10-11 10:09:50 -0400

    Added writeup from Akua

--- R/utilities.R
+++ R/utilities.R
@@ -1,9 +1,45 @@
-#' drop variables with variance below a certain threshold
+#' Remove non-numeric characters from a string
 #'
-#' @param dataset A dataframe
-#' @param threshold A number; variance threshold below which variables
-#' @return dataset with same name but with variables with variance below threshold dropped.
+#' @param string string
+#'
 #' @export
-drop_if_variance_below <- function(dataset, threshold) {
-  subset(dataset, select = -as.numeric(which(apply(dataset, 2, stats::var) <= threshold)))
+#'
+remove_non_num <- function(string) {
+  gsub('[^0-9.-]', '', string)
 }
+
+#' Convert non-numeric variables and coerce to numeric
+#'
+#' @param string string
+#'
+#' @export
+#'
+coerce_num_cln <- function(string) {
+  as.numeric(gsub('[^0-9.-]', '', string))
+}
+
+#' Select columns of numeric variables
+#'
+#' @param dat dataframe
+#'
+#' @export
+#'
+select_num_vars <- function(dat) {
+  subset(dat, select = sapply(dat, is.numeric))
+}
+
+#' Remove columns which are all the same value for numeric variables
+#'
+#' @param dat dataframe
+#'
+#' @export
+#'
+rem_zero_var_cols <- function(dat) {
+  subset(dat, select = -as.numeric(which(apply(dat, 2, var) == 0)))
+}
+
+#' #' @export
+#' #'
+#' combine_vars <- function(varlist) {
+#'   lapply(varlist, function(x) paste0(names(x), varlist)))
+#' }
