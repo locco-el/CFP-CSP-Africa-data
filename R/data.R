eade8c92d7ff97560cc849492315e144f92c81f1
Oduro Oppong-Nkrumah <locco.el@gmail.com>
2020-10-15 10:53:24 -0400

    Separating data cleaning from analysis into separate projects

--- R/data.R
+++ R/data.R
@@ -1,4 +1,48 @@
-#' CSP and CFP data for selected African businesses
+#' CSP data for PCA
+#'
+#' A dataset containing variables on CSP domains, and company characteristics
+#' for 238 African companies spanning a period of 5 years (2014 to 2018)
+#'
+#' Note: some data was used from companies in the period before they
+#' were officially listed  on the stock exchange
+#'
+#' @format A data frame with 1195 rows and 110 variables:
+#'
+#' \describe{
+#'   \item{Sector}{Categorization of industry, 9 categories}
+#'   \item{Country}{Listing Country}
+#'   \item{n_com_educ}{Education}
+#'   \item{n_com_hlth}{Education}
+#'   \item{n_com_art}{Whether multinational, binary}
+#'   \item{n_com_econ}{Specific industry, 30 categories}
+#'   \item{n_com_agric}{Year of report, 2014 - 2018}
+#'   \item{n_com_activism}{Year company listed on stock exchange, 1927 - 2018}
+#'   \item{n_hr_safety}{Year company incorporated, 1852 - 2012}
+#'   \item{n_hr_diversity}{Net profit reported, different various currencies}
+#'   \item{n_hr_training}{Reported total assets at end of year, various local currencies}
+#'   \item{n_hr_benefits}{Book value of equity, various local currencies}
+#'   \item{n_hr_monitoring}{Estimated market value, various local currencies}
+#'   \item{n_hr_satisfaction}{Community CSR variables, 22, 5-category variables}
+#'   \item{n_prod_info}{Human resource CSR variables, 30, 5-category variables}
+#'   \item{n_prod_quality}{... CSR variables, 30, 5-category variables}
+#'   \item{n_ene_conservation}{Environmental CSR variables, 30, 5-category variables}
+#'   \item{n_ene_policy}{Environmental CSR variables, 30, 5-category variables}
+#'   \item{n_emi_policy}{... CSR variables, 30, 5-category variables}
+#'   \item{n_emi_practice}{... CSR variables, 10, 5-category variables}
+#'   \item{n_env_pollution}{Unclassified CSR variables, 6, 5-category variables}
+#'   \item{n_env_conservation}{Existence of a social and evnironmental committee on board, binary}
+#'   \item{n_env_policy}{Industrial sector relassification, 3 categories}
+#'   \item{n_env_water}{Industrial sector relassification, 3 categories}
+#'   \item{n_oth_other}{Industrial sector relassification, 3 categories}
+#' }
+#'
+#' @source Data gathered from published company annual reports
+#'
+"pca_data"
+
+
+
+#' CFP and organizational charactersitics data
 #'
 #' A dataset containing variables on CFP, CSR, and company characteristics
 #' for 238 African companies spanning a period of 5 years (2014 to 2018)
@@ -7,28 +51,33 @@
 #' were officially listed  on teh stock exchange
 #'
 #' @format A data frame with 1195 rows and 110 variables:
+#'
 #' \describe{
-#'   \item{sector}{Categorization of industry, 9 categories}
-#'   \item{company}{company name}
-#'   \item{listing_country}{Country of listing}
-#'   \item{locality}{Whether multinational, binary}
-#'   \item{industry}{Specific industry, 30 categories}
-#'   \item{reporting_year}{Year of report, 2014 - 2018}
-#'   \item{listing_year}{Year company listed on stock exchange, 1927 - 2018}
-#'   \item{incorporation_year}{Year company incorporated, 1852 - 2012}
-#'   \item{net_prof}{Net profit reported, different various currencies}
-#'   \item{tot_asst}{Reported total assets at end of year, various local currencies}
-#'   \item{bk_eqt}{Book value of equity, various local currencies}
-#'   \item{market_value}{Estimated market value, various local currencies}
-#'   \item{com_}{Community CSR variables, 22, 5-category variables}
-#'   \item{hr_}{Human resource CSR variables, 30, 5-category variables}
-#'   \item{prod_}{... CSR variables, 30, 5-category variables}
-#'   \item{env_}{Environmental CSR variables, 30, 5-category variables}
-#'   \item{ene_}{... CSR variables, 30, 5-category variables}
-#'   \item{emi_}{... CSR variables, 10, 5-category variables}
-#'   \item{oth_}{Unclassified CSR variables, 6, 5-category variables}
-#'   \item{social_commitee}{Existence of a social and evnironmental committee on board, binary}
-#'   \item{sector_class}{Industrial sector relassification, 3 categories}
+#'   \item{Country}{Categorization of industry, 9 categories}
+#'   \item{Multinational}{company name}
+#'   \item{Sector}{Country of listing}
+#'   \item{listing_age}{Whether multinational, binary}
+#'   \item{incorp_age}{Specific industry, 30 categories}
+#'   \item{lag_return_on_assets}{Year of report, 2014 - 2018}
+#'   \item{lag_return_on_equity}{Year company listed on stock exchange, 1927 - 2018}
+#'   \item{social_committee}{Year company incorporated, 1852 - 2012}
+#'   \item{reporting_year}{Net profit reported, different various currencies}
 #' }
+#'
 #' @source Data gathered from published company annual reports
-"all_data"
+#'
+"reg_data"
+
+#' CFP and organizational charactersitics data
+#'
+#' A dataset containing variables on CFP, CSR, and company characteristics
+#' for 238 African companies spanning a period of 5 years (2014 to 2018)
+#'
+#' Note: some data was used from companies in the period before they
+#' were officially listed  on teh stock exchange
+#'
+#' @format A data frame with 1195 rows and 110 variables:
+#'
+#' @source Data gathered from published company annual reports
+#'
+"pca_ind_scores"
